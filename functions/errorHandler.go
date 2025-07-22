efcf9178ec1017c58ee91b7915ef7ca033f273b8
fhosaily <fadlo.hosaily@gmail.com>
2025-07-22 09:53:44 +0100

    handling-errors

---
+++ functions/errorHandler.go
@@ -0,0 +1,21 @@
+package functions
+
+import (
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func ErrorHandler(w http.ResponseWriter, text string, state int) {
+	tmpl, err := template.ParseFiles("templates/error.html")
+	if err != nil {
+		http.Error(w, "Erreur lors du chargement de la page", http.StatusInternalServerError)
+		return
+	}
+	data := map[string]string{
+		"errorText": text,
+		"statue":    strconv.Itoa(state),
+	}
+	w.WriteHeader(state)
+	tmpl.Execute(w, data)
+}
