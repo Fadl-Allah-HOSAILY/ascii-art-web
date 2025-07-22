73390e8f6660c884c11a5d19d7d536639094381f
jazahraoui <zahraoui.jawad8@gmail.com>
2025-07-22 23:32:45 +0100

    staticHandler

---
+++ functions/staticHandler.go
@@ -0,0 +1,31 @@
+package functions
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func HandleStatic(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		ErrorHandler(w,  "Method not allowed !", http.StatusMethodNotAllowed,)
+		return
+	}
+	if !strings.HasPrefix(r.URL.Path, "/static") {
+		ErrorHandler(w,  "Statut not found !", http.StatusNotFound,)
+		return
+	} else {
+		fmt.Println(r.URL.Path)
+		infos, err := os.Stat(r.URL.Path[1:])
+		if err != nil {
+			ErrorHandler(w,  "Statut not found !", http.StatusNotFound,)
+			return
+		} else if infos.IsDir() {
+			ErrorHandler(w,  "Access Forbidden !", http.StatusForbidden,)
+			return
+		} else {
+			http.ServeFile(w, r, r.URL.Path[1:])
+		}
+	}
+}
--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	mux.HandleFunc("/", functions.HandlerIndex)
 	mux.HandleFunc("/ascii-art", functions.HandlerPost)
 	log.Println("Server running on: http://localhost:8080")
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux.HandleFunc("/static/", functions.HandleStatic)
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal("Erreur serveur :", err)
